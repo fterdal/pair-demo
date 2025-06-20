f1ee66a613ada097eb96c10d6f2a1aef5f4707a1
Shahid Sarker <shahid.sarker@gmail.com>
2025-06-20 10:47:04 -0400

    Implement myReduce function

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 // reduce function
 // reduce(callbackFn, initialValue)
 // myReduce(array, callback, initialVal)
+function myReduce(array, callback, initialVal) {
+  let result = initialVal;
+  let startingIdx = 0;
+  if (initialVal == undefined) {
+    startingIdx = 1;
+    result = array[0];
+  }
+  for (let i = startingIdx; i < array.length; i++) {
+    result = callback(result, array[i]);
+  }
+  return result;
+}
+
+const arrayOne = [1, 2, 3];
+
+const callbackOne = (accumulator, currentVal) => {
+  return accumulator + currentVal;
+};
+
+console.log(myReduce(arrayOne, callbackOne, 15));
+
+const arrayTwo = ["h", "e", "l", "l", "o"];
+
+const callbackTwo = (acc, cur) => {
+  return acc + cur;
+};
+
+console.log(myReduce(arrayTwo, callbackTwo, 'world'));
